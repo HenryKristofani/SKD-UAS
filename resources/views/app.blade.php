c9531127a56b00f24022ed2f576d0f879904180e
HenryKristofani <henry.kristofani@gmail.com>
2024-12-21 01:00:17 +0700

    PUSH
    title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,17 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title>MENDASKY - Bukit Mongkrang</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/icon.png') }}" type="image/png">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <head>
-        <!-- Font Awesome CDN -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
-    </head>
-
+    <!-- Font Awesome CDN -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 
     <!-- Scripts -->
     @routes
